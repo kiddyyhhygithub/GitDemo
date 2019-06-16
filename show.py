479117348c12d8d9e17c11e29056642632efaf63
kiddyyhhygithub <kiddyflash@163.com>
2019-06-16 21:38:47 +0800

    1st

---
+++ show.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/6/16 0016 下午 9:38
+# @Author  : Richard
+# @Site    : 
+# @File    : show.py
+# @email   : kiddyflash@163.com
+
+def show():
+    return None
